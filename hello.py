43a517e52bf03285bd69bc07c1d8d380e823072f
YuhuiJiang99 <yuhui.jiang@outlook.com>
2021-09-23 17:41:13 -0400

    Complete Activity3

---
+++ hello.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+from datetime import datetime
+
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+
+
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+
+
+@app.route('/')
+def index():
+    return '<h1>Hello World!</h1>'
+
+
+@app.route('/user/<name>')
+def user(name):
+    print(datetime.utcnow())
+    return render_template('user.html', name=name, current_time=datetime.utcnow())
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
